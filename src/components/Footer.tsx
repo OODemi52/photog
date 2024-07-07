2baabf37dfef78837efb4854bd3b2385c6fad477
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2024-07-06 21:50:11 -0400

    Added title to instagram logo for accesibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="flex flex-col items-center justify-around w-screen py-4 h-28 mt-24 border-t bg-zinc-900">
-      <Link href="https://www.instagram.com/d__labs?igsh=MTNudHI2eDg0czlsbg%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer">
+      <Link href="https://www.instagram.com/d__labs?igsh=MTNudHI2eDg0czlsbg%3D%3D&utm_source=qr" title="Instagram - D-Labs Photography" target="_blank" rel="noopener noreferrer">
         <InstagramLogoIcon className="h-6 w-6" />
       </Link>
       <p className="text-sm font-thin">© 2024 D-Labs Photography </p>
